aa427b56b244c4113931b26b9baaa7445ee39012
mo-laurenboon <lauren.boon@metoffice.gov.uk>
2025-11-10 12:05:31 +0000

    syntax error fix

--- generate_all_templates.py
+++ generate_all_templates.py
@@ -184,7 +184,7 @@ DATA = {
         "ocean": {"id": "ocean", "label": "ocean"},
         "ocnbgchem": {"id": "ocnbgchem", "label": "ocnbgchem"},
         "seaice": {"id": "seaice", "label": "seaice"}
-    }
+    },
     "modifiers": ["new","modify"]
 }
 """
