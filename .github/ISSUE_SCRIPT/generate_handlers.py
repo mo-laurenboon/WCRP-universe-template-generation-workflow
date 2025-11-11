df1dcc8f219aa2db0cf2a7c12a4a83be445830a7
cmip-ipo <actions@wcrp-cmip.org>
2025-11-11 16:10:27 +0000

    Clean file upload from src-data

--- .github/ISSUE_SCRIPT/generate_handlers.py
+++ .github/ISSUE_SCRIPT/generate_handlers.py
@@ -66,9 +66,14 @@ def create_{{ category_safe }}_json(data):
     #Map fields from the issue form to JSON structure
     for key, value in data.items():
         if key not in ["issue-type", "issue--kind"] and value:
+            if key == "type":
+                dictionary["@type"] = dictionary.pop("type")
+            if key == "id":
+                dictionary["@id"] = dictionary.pop("id")
             # Convert field names back to JSON format
             json_key = key.replace("_", "-")
             result[json_key]=value
+    result["@context"] = "_context"
 
     return result
 
